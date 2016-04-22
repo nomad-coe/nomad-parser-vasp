9e344c4f2cb0ea2878fb55a3a2a86273025681ee
Fawzi Mohamed <fawzi.mohamed@fhi-berlin.mpg.de>
2016-04-22 12:47:49 +0200

    separating out incar out, fixing XC if "--"

--- parser/parser-vasp/parser_vasprun.py
+++ parser/parser-vasp/parser_vasprun.py
@@ -76,7 +76,7 @@ class VasprunContext(object):
         if pdate:
             backend.addValue("program_compilation_datetime", secondsFromEpoch(pdate))
         for i in element:
-            if i.tag != "i" or not i.attrib.get("name") in set(["name", "version", "subversion", "platform", "program_version", "date", "time"]):
+            if i.tag != "i" or not i.attrib.get("name") in set(["program", "version", "subversion", "platform", "program_version", "date", "time"]):
                 backend.pwarn("unexpected tag %s %s %r in generator" % (i.tag, i.attrib, i.text))
 
     def onEnd_incar(self, parser, event, element):
@@ -232,7 +232,7 @@ class VasprunContext(object):
                     if self.bands:
                         divisions = int(self.bands['divisions'])
                         backend.openNonOverlappingSection("section_k_band")
-                        backend.addArrayValues("band_energies", np.reshape(ev, (self.kpoints.shape[0]/divisions, self.ispin, divisions ,  bands.shape[0])))
+                        backend.addArrayValues("band_energies", np.reshape(ev, (self.ispin, self.kpoints.shape[0]/divisions, divisions ,  bands.shape[0])))
                         backend.closeNonOverlappingSection("section_k_band")
                     else:
                         backend.openNonOverlappingSection("section_eigenvalues_group")
@@ -286,6 +286,7 @@ class VasprunContext(object):
         nAtomTypes = None
         atomTypes = []
         labels = []
+        atomTypesDesc = []
         for el in element:
             if el.tag == "atoms":
                 nAtoms = int(el.text.strip())
@@ -310,13 +311,114 @@ class VasprunContext(object):
                         else:
                             backend.pwarn("unexpected tag %s in atoms array in atominfo" % atomsEl.tag)
                 elif name == "atomtypes":
-                    pass
-                else:
-                    backend.pwarn("unexpected array named %s in atominfo" % name)
+                    keys = []
+                    fieldTypes = []
+                    for atomsEl in el:
+                        if atomsEl.tag == "dimension":
+                            pass
+                        elif atomsEl.tag == "field":
+                            keys.append(atomsEl.text.strip())
+                            fieldTypes.append(atomsEl.attrib.get("type", "float"))
+                        elif atomsEl.tag == "set":
+                            expectedKeys = ["atomspertype", "element", "mass", "valence", "pseudopotential"]
+                            if keys != expectedKeys:
+                                backend.pwarn("unexpected fields in atomtype: %s vs %s" % (keys, expectedKeys))
+                            for atomsLine in atomsEl:
+                                if atomsLine.tag != "rc":
+                                    backend.pwarn("unexpected tag %s in atoms array in atominfo" % atomsLine.tag)
+                                else:
+                                    line = atomsLine.findall("c")
+                                    typeDesc = {}
+                                    for i, k in enumerate(keys):
+                                        fieldType = fieldTypes[i]
+                                        value = line[i].text
+                                        if fieldType == "float":
+                                            value = float(value)
+                                        elif fieldType == "int":
+                                            value = int(value)
+                                        else:
+                                            pass
+                                        typeDesc[k] = value
+                                    atomTypesDesc.append(typeDesc)
+                        else:
+                            backend.pwarn("unexpected tag %s in atomtypes array in atominfo" % atomsEl.tag)
+                    else:
+                        backend.pwarn("unexpected array named %s in atominfo" % name)
             else:
                 backend.pwarn("unexpected tag %s in atominfo" % el.tag)
         if labels:
             backend.addValue("atom_label", labels)
+
+    def incarOutTag(self, el):
+        backend = parser.backend
+        metaEnv = parser.backend.metaInfoEnv()
+        if (el.tag != "i"):
+            backend.pwarn("unexpected tag %s %s %r in incar" % (el.tag, el.attrib, el.text))
+        else:
+            name = el.attrib.get("name", None)
+            meta = metaEnv['x_vasp_incarOut_' + name]
+            valType = el.attrib.get("type")
+            if not meta:
+                backend.pwarn("Unknown INCAR out parameter (not registered in the meta data): %s %s %r" % (el.tag, el.attrib, el.text))
+            elif valType:
+                expectedMetaType = {
+                    'string': ['C'],
+                    'int': ['i'],
+                    'logical': ['b','C']
+                }.get(valType)
+                if not expectedMetaType:
+                    backend.pwarn("Unknown value type %s encountered in INCAR out: %s %s %r" % (valType, el.tag, el.attrib, el.text))
+                elif not meta.get('dtypeStr') in expectedMetaType:
+                    backend.pwarn("type mismatch between meta data %s and INCAR type %s for %s %s %r" % ( meta.get('dtypeStr'), valType, el.tag, el.attrib, el.text))
+                else:
+                    shape = meta.get("shape", None)
+                    dtypeStr = meta.get("dtypeStr", None)
+                    converter = metaTypeTransformers.get(dtypeStr)
+                    if not converter:
+                        backend.pwarn("could not find converter for dtypeStr %s when handling meta info %s" % (dtypeStr, ))
+                    elif shape:
+                        vals = re.split("\s+", el.text.strip())
+                        backend.addValue(meta["name"], map(converter, vals))
+                    else:
+                        backend.addValue(meta["name"], converter(el.text))
+                if name == 'GGA':
+                    fMap = {
+                        '91': ['GGA_X_PW91', 'GGA_C_PW91'],
+                        'PE': ['GGA_X_PBE', 'GGA_C_PBE'],
+                        'RP': ['GGA_X_RPBE', 'GGA_C_PBE'],
+                        'PS': ['GGA_C_PBE_SOL', 'GGA_X_PBE_SOL'],
+                        '--': ['GGA_X_PBE', 'GGA_C_PBE'] # should check potcar
+                    }
+                    functs = fMap.get(el.text.strip(), None)
+                    if not functs:
+                        backend.pwarn("Unknown XC functional %s" % el.text.strip())
+                    else:
+                        for f in functs:
+                            backend.openNonOverlappingSection("section_XC_functionals")
+                            backend.addValue("XC_functional_name", f)
+                            backend.closeNonOverlappingSection("section_XC_functionals")
+                elif name == "ISPIN":
+                    self.ispin = int(el.text.strip())
+
+    def separatorScan(self, element, depth = 0):
+        for separators in element:
+            if separators.tag == "separator":
+                separatorName = separators.attrib.get("name")
+                for el in separators:
+                    if el.tag == "i":
+                        self.incarOutTag(el)
+                    elif el.tag == "separator":
+                        self.separatorScan(el, depth + 1)
+                    else:
+                        backend.pwarn("unexpected tag %s %s in parameters separator %s at depth %d" % (el.tag, el.attrib, separatorName, depth))
+            elif separators.tag == "i":
+                self.incarOutTag(separators)
+            else:
+                backend.pwarn("unexpected tag %s %s in parameters at depth %d" % (separators.tag, separators.attrib, depth))
+
+    def onEnd_parameters(self, parser, event, element):
+        backend = parser.backend
+        self.separatorScan(element)
 
 class XmlParser(object):
     @staticmethod
