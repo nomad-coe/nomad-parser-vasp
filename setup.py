7c97a280a4c04ffdd223be637886ca7b86355a7a
Markus Scheidgen <markus.scheidgen@gmail.com>
2018-09-21 14:20:01 +0200

    Fixed setup to find packages.

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 # Copyright 2016-2018 Fawzi Mohamed, Lauri Himanen, Danio Brambila, Ankit Kariryaa, Henning Glawe
-# 
+#
 #   Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
 #   You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 #   Unless required by applicable law or agreed to in writing, software
 #   distributed under the License is distributed on an "AS IS" BASIS,
 #   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,17 +17,18 @@ from setuptools import setup, find_packages
 
 def main():
     setup(
-        name="vaspparser",
-        version="0.1",
-        description="NOMAD parser implementation for VASP.",
-        author="Fawzi Mohamed",
-        license="",
+        name='vaspparser',
+        version='0.1',
+        description='NOMAD parser implementation for VASP.',
+        author='Fawzi Mohamed',
+        license='APACHE 2.0',
         package_dir={'': 'parser/parser-vasp'},
-        packages=find_packages(),
+        packages=find_packages('parser/parser-vasp'),
         install_requires=[
             'nomadcore'
         ],
     )
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     main()
